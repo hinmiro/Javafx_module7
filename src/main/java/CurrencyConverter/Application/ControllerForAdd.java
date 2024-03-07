201a83de6ff36b468ef1feff456a535bd9cc6226
mirohi <m_hintikka@hotmail.com>
2024-03-07 15:09:29 +0200

    module7.3 tasks update

--- src/main/java/CurrencyConverter/Application/ControllerForAdd.java
+++ src/main/java/CurrencyConverter/Application/ControllerForAdd.java
@@ -7,26 +7,24 @@ import javafx.scene.control.*;
 
 public class ControllerForAdd {
     @FXML
-    private Button addRateM;
-    @FXML
     private TextField newCurrency;
     @FXML
     private TextField newCurrencyRate;
     private CurrencyConverter gui;
     private Controller cont;
-    private double rate;
 
     public void addNewRate() {
+        CurrencyDao dao = new CurrencyDao();
+
         try {
-            rate = Double.parseDouble(newCurrencyRate.getText().replace(",", "."));
+            double rate = Double.parseDouble(newCurrencyRate.getText().replace(",", "."));
+            dao.persist(new Currency(rate, newCurrency.getText()));
         } catch (NumberFormatException e) {
             System.out.println("Invalid currency rate");
             newCurrencyRate.clear();
             e.printStackTrace();
             return;
         }
-        CurrencyDao dao = new CurrencyDao();
-        dao.persist(new Currency(Double.parseDouble(newCurrencyRate.getText()), newCurrency.getText()));
         System.out.println("New currency Added");
         this.cont.updateComboBox();
         gui.closeAddStage();
